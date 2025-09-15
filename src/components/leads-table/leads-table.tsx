ca8119ad933db33a4795f1a5a8e69286021c9f0a
Guilherme <guilherme@alume.com>
2025-09-15 20:06:23 -0300

    feat: filter by name or company; sort score by desc

--- src/components/leads-table/leads-table.tsx
+++ src/components/leads-table/leads-table.tsx
@@ -2,17 +2,42 @@ import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from '@
 import { getLeads } from '@/lib/api'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { LeadsTableHeader } from './leads-table-header'
+import { useSearchParams } from 'react-router-dom'
+import { useMemo } from 'react'
 
 export function LeadsTable() {
+  const [searchParams] = useSearchParams()
+
+  const status = searchParams.get('status')
+  const query = searchParams.get('q')
+
   const { data: leads } = useSuspenseQuery({
     queryKey: ['leads'],
     queryFn: getLeads,
   })
 
+  const filteredLeads = useMemo(() => {
+    let filteredLeads = leads
+
+    if (status) {
+      filteredLeads = filteredLeads.filter((lead) => status.includes(lead.status))
+    }
+
+    if (query) {
+      filteredLeads = filteredLeads.filter(({ name, company }) => name.toLowerCase().includes(query.toLowerCase()) || company.toLowerCase().includes(query.toLowerCase()))
+    }
+
+    return filteredLeads
+  }, [leads, status, query])
+
+  const sortedLeads = useMemo(() => {
+    return [...filteredLeads].sort((a, b) => b.score - a.score)
+  }, [filteredLeads])
+
   return (
     <div>
       <LeadsTableHeader />
-      
+
       <div className="overflow-hidden rounded-md border">
         <Table>
           <TableHeader>
@@ -27,7 +52,12 @@ export function LeadsTable() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {leads.map((lead) => (
+            {sortedLeads.length === 0 && (
+              <TableRow>
+                <TableCell colSpan={7} className="text-center h-[300px]">No leads found</TableCell>
+              </TableRow>
+            )}
+            {sortedLeads.map((lead) => (
               <TableRow key={lead.id}>
                 <TableCell>{lead.id}</TableCell>
                 <TableCell>{lead.name}</TableCell>
