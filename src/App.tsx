0f48d804975c66bc67c093ecae637c17fdd8e481
Guilherme <guilherme@alume.com>
2025-09-15 17:40:25 -0300

    wip: creating leads table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import './App.css'
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from '@/components/ui/table'
+import leads from '@/leads.json'
+
 
 function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello World</h1>
+    <div className="container mx-auto px-6">
+      <h1 className='text-5xl font-bold text-center my-10'>Mini Seller Console</h1>
+
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Company</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Source</TableHead>
+              <TableHead>Score</TableHead>
+              <TableHead>Status</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {leads.map((lead) => (
+              <TableRow key={lead.id}>
+                <TableCell>{lead.id}</TableCell>
+                <TableCell>{lead.name}</TableCell>
+                <TableCell>{lead.company}</TableCell>
+                <TableCell>{lead.email}</TableCell>
+                <TableCell>{lead.source}</TableCell>
+                <TableCell>{lead.score}</TableCell>
+                <TableCell>{lead.status}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }
