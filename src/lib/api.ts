8448ee4b06ed66b85a01d14476b1cab61f697e12
Guilherme <guilherme@alume.com>
2025-09-16 20:43:37 -0300

    feat: get opportunities from api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,15 @@
 import { delay } from "./utils"
 import leads from "@/leads.json"
 import type { Lead } from "@/types/lead"
+import opportunities from "@/opportunities.json"
+import type { Opportunity } from "@/types/opportunity"
 
 export const getLeads = async (): Promise<Lead[]> => {
   await delay(2000)
   return leads as Lead[]
+}
+
+export const getOpportunities = async (): Promise<Opportunity[]> => {
+  await delay(1000)
+  return opportunities as Opportunity[]
 }
