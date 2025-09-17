8dc68c0b45a4b9387031f8d8e705a1fa736ff435
Guilherme <guilherme@alume.com>
2025-09-17 14:21:23 -0300

    chore: disable retry on queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    }
+  }
+})
 
 function App() {
   return (
